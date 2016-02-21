71525f7f8109fe0848535a6b28e2c8e34262b2ff
Armando Cerna <armandocerna@gmail.com>
2016-02-20 23:26:06 -0800

    Add yank binding to copy to system clipboard

---
+++ setupdots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ~/.tmp/{undo,backup,swap}
+mkdir -p ~/.vim
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/colors ~/.vim/colors
+
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+ln -s ~/dotfiles/xresources/.Xresources ~/.Xresources
+
