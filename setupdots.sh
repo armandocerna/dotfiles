5c8b23d24464c9f7c9202d93b886d1a859d6abcf
Armando Cerna <armandocerna@gmail.com>
2016-03-05 02:29:48 -0500

    Add prezto to dot setup script

--- setupdots.sh
+++ setupdots.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 mkdir -p ~/.tmp/{undo,backup,swap}
 mkdir -p ~/.vim
+mkdir -p ~/.config
+ln -sf ~/.vim ~/.config/nvim
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/colors ~/.vim/colors
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/prezto/zlogin ~/.zlogin
-ln -s ~/dotfiles/prezto/zlogout ~/.zlogout
-ln -s ~/dotfiles/prezto/zpreztorc ~/.zpreztorc
-ln -s ~/dotfiles/prezto/zprofile ~/.zprofile
-ln -s ~/dotfiles/prezto/zshenv ~/.zshenv
-ln -s ~/dotfiles/prezto/zshrc ~/.zshrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/prezto/zlogin ~/.zlogin
+ln -sf ~/dotfiles/prezto/zlogout ~/.zlogout
+ln -sf ~/dotfiles/prezto/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/prezto/zprofile ~/.zprofile
+ln -sf ~/dotfiles/prezto/zshenv ~/.zshenv
+ln -sf ~/dotfiles/prezto/zshrc ~/.zshrc
