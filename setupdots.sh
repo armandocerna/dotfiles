2d39a3ab6d9ca3b4cc1859951ced1cec94d8f54a
Armando Cerna <armandocerna@gmail.com>
2016-03-04 20:49:49 -0800

    modified dotfile setup script to add new symlinks for prezto

--- setupdots.sh
+++ setupdots.sh
@@ -3,7 +3,12 @@ mkdir -p ~/.tmp/{undo,backup,swap}
 mkdir -p ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/colors ~/.vim/colors
-ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/prezto/zlogin ~/.zlogin
+ln -s ~/dotfiles/prezto/zlogout ~/.zlogout
+ln -s ~/dotfiles/prezto/zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/prezto/zprofile ~/.zprofile
+ln -s ~/dotfiles/prezto/zshenv ~/.zshenv
+ln -s ~/dotfiles/prezto/zshrc ~/.zshrc
