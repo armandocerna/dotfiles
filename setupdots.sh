b94e6358f211d00a98ed3fe0243ab4ca143fa4db
Armando Cerna <armandocerna@gmail.com>
2016-03-04 23:04:47 -0500

    Automatically grab vim plug

--- setupdots.sh
+++ setupdots.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir -p ~/.tmp/{undo,backup,swap}
 mkdir -p ~/.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/colors ~/.vim/colors
-
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-
 
