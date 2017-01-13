c662ec4a28de6dd90bc5849c8f68e914943040c0
Armando Cerna <armandocerna@gmail.com>
2017-01-13 08:45:00 -0800

    remove keyboard script run from docking script

--- scripts/dock.rb
+++ scripts/dock.rb
@@ -12,5 +12,4 @@ else
   system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
 end
 
-system("keyboard_shit.sh")
 system("wall.sh")
