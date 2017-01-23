0dc1c6a8790646d374a62ab0c3091fdbc4e73885
Armando Cerna <armandocerna@gmail.com>
2017-01-22 22:40:01 -0800

    Add additional dotfiles and force to symlinks

--- setupdots.sh
+++ setupdots.sh
@@ -2,6 +2,7 @@
 mkdir -p ~/.tmp/{undo,backup,swap}
 mkdir -p ~/.vim
 mkdir -p ~/.config
+mkdir -p ~/.config/fontconfig
 ln -sf ~/.vim ~/.config/nvim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -9,7 +10,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
-ln -s ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/xresources/Xresources ~/.Xresources
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/tmux/tmux-theme.conf ~/.tmux-theme.conf
 ln -sf ~/dotfiles/prezto/zlogin ~/.zlogin
@@ -19,3 +21,5 @@ ln -sf ~/dotfiles/prezto/zprofile ~/.zprofile
 ln -sf ~/dotfiles/prezto/zshenv ~/.zshenv
 ln -sf ~/dotfiles/prezto/zshrc ~/.zshrc
 ln -sf ~/dotfiles/prezto/aliases.sh ~/.aliases.sh
+ln -sf ~/dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+sudo ln -sf ~/dotfiles/scripts/dock.rb /usr/local/bin/dock.rb
