99dee9b4bd558595cb77de01b880e573b20388d9
Armando Cerna <armandocerna@gmail.com>
2017-01-22 22:39:22 -0800

    fix display names for intel drivers

--- scripts/dock.rb
+++ scripts/dock.rb
@@ -5,11 +5,11 @@ docked = system("lsusb | grep Lenovo")
 
 if docked
   # Above
-  system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
   # Left
   # system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x592 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 1792x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
 else
-  system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
 end
 
 system("feh --randomize --bg-fill ~/.wallpaper/*")
