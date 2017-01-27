a9033571a2b1f0720eb654c6f8d1787a4cd67ce5
Armando Cerna <armandocerna@gmail.com>
2017-01-26 20:11:29 -0800

    Add to dotfile script change dispalys to use modesetting driver

--- scripts/dock.rb
+++ scripts/dock.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 docked = system("lsusb | grep Lenovo")
-# system("xrandr --output eDP1 --scale .7x.7")
+# system("xrandr --output eDP-1 --scale .7x.7")
 
 if docked
   # Above
-  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
   # Left
-  # system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x592 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 1792x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
+  # system("xrandr --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x592 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --mode 2560x1080 --pos 1792x0 --rotate normal --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
 else
-  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
 end
 
 system("feh --randomize --bg-fill ~/.wallpaper/*")
