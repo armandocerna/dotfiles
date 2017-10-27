3a419d8bf8f2c0bbd579a2da8882c4072c38551a
Armando Cerna <armando@cerna.ca>
2017-10-27 14:18:44 -0700

    minor fixes and add sway

--- scripts/dock.rb
+++ scripts/dock.rb
@@ -1,23 +1,24 @@
 #!/usr/bin/env ruby
 
 docked = system("lsusb | grep Lenovo")
-# system("xrandr --output eDP-1 --scale .7x.7")
+system("xrandr --output eDP1 --scale 1x1")
 
 if docked
   # Two Display + Laptop
-  # system("xrandr --output DP-2-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2-2 --mode 2560x1080 --pos 2560x360 --rotate normal --output DP-2-3 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off")
+  # system("xrandr --output DP2-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP2-2 --mode 2560x1080 --pos 2560x360 --rotate normal --output DP2-3 --off --output eDP1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP2 --off --output DP1 --off")
   # Two Display Laptop closed
-  # system("xrandr --output DP-2-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2-2 --mode 2560x1080 --pos 2560x360 --rotate normal --output DP-2-3 --off --output eDP-1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off")
+  # system("xrandr --output DP2-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2-2 --mode 2560x1080 --pos 2560x360 --rotate normal --output DP2-3 --off --output eDP1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP2 --off --output DP1 --off")
   # Above
-  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
   # Left
-  # system("xrandr --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x592 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --mode 2560x1080 --pos 1792x0 --rotate normal --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
+  # system("xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x592 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 1792x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
 else
-  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP-2 --off")
+  system("xrandr -d :0.0 --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off")
 end
 
 system("pkill -f compton")
-system("compton -b --backend=glx")
+# system("compton -b --backend=glx")
+system("compton -b")
 system("feh --randomize --bg-fill ~/.wallpaper/*")
 # system("wal -i ~/.wallpaper -t")
 
