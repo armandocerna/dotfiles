1402f1c34fc6456a00dce7d1227f0b9fee429d1e
Armando Cerna <armando@mavenlink.com>
2018-12-18 10:15:20 -0800

    misc updates

--- bin/kube.sh
+++ bin/kube.sh
@@ -2,11 +2,11 @@
 
 case $1 in
   "up")
-    sudo swapoff -a
-    sudo sysctl net.bridge.bridge-nf-call-iptables=1
+    # sudo swapoff -a
+    # sudo sysctl net.bridge.bridge-nf-call-iptables=1
     sudo kubeadm init
     mkdir -p $HOME/.kube
-    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown armando. $HOME/.kube/config
     kubectl taint nodes --all node-role.kubernetes.io/master-
     kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar laptop &
-polybar desk &
+polybar bar0 &
 
 
 echo "Bars launched..."
