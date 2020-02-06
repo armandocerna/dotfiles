c9d85bb4972c5b649022cb1d680b3fd6bc94f7a6
Armando Cerna <armando@cerna.ca>
2020-02-06 06:07:25 -0800

    assorted changes

--- setupdots.sh
+++ setupdots.sh
@@ -2,8 +2,6 @@
 mkdir -p ~/.tmp/{undo,backup,swap}
 mkdir -p ~/.vim
 mkdir -p ~/.config
-mkdir -p ~/.config/fontconfig
-mkdir -p ~/.config/i3
 ln -sf ~/.vim ~/.config/nvim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,7 +10,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/vim/colors ~/.vim/colors
-ln -sf ~/dotfiles/xresources/Xresources ~/.Xresources
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/tmux/tmux-theme.conf ~/.tmux-theme.conf
 ln -sf ~/dotfiles/prezto/zlogin ~/.zlogin
@@ -22,10 +19,4 @@ ln -sf ~/dotfiles/prezto/zprofile ~/.zprofile
 ln -sf ~/dotfiles/prezto/zshenv ~/.zshenv
 ln -sf ~/dotfiles/prezto/zshrc ~/.zshrc
 ln -sf ~/dotfiles/prezto/aliases.sh ~/.aliases.sh
-ln -sf ~/dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
-ln -sf ~/dotfiles/i3/config ~/.config/i3/config
-ln -sf ~/dotfiles/i3blocks/config ~/.i3blocks.conf
-ln -sf ~/dotfiles/xmodmap/xmodmap ~/.Xmodmap
 ln -sf ~/dotfiles/intellij/ideavimrc ~/.ideavimrc
-
-sudo ln -sf ~/dotfiles/scripts/dock.rb /usr/local/bin/dock.rb
