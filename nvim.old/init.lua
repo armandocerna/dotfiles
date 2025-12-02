7fc3387111fee9f0e26e06dbf5d8cab498912281
Armando Cerna <armando@sanity.io>
2025-12-02 16:18:24 -0500

    lazy

---
+++ nvim.old/init.lua
@@ -0,0 +1,4 @@
+require("plugins")
+require("config")
+require("keymaps")
+require("lsp")
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,2 @@
-require("plugins")
-require("config")
-require("keymaps")
-require("lsp")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
